42eadc013eb2308e966c3bb870c3c741ca7a9e93
Tarik Kdiry <tarik.kdiry@gmail.com>
2018-09-01 14:59:01 -0400

    hw_01

---
+++ hw_01/hw_01.py
@@ -0,0 +1,34 @@
+#Classifies triangle based on side lengths
+triangle = ""
+side_list = []
+
+def classify_triangle(a, b, c):
+
+    side_list = [a, b, c]
+    side_count = []
+    count = 0
+    a = side_list[0]
+    b = side_list[1]
+    c = side_list[2]
+
+    for x in range(len(side_list)):
+        for y in range(len(side_list)):
+            if (side_list[x] == side_list[y]):
+                count += 1
+        side_count.append(count)
+        count = 0
+
+    if ((a**2) + (b**2) == (c**2)):
+        triangle = "right"
+    else:
+        if (max(side_count) == 1):
+            triangle = "scalene"
+        elif (max(side_count) == 2):
+            triangle = "isosceles"
+        else:
+            triangle = "equilateral"
+          
+    return triangle
+
+
+print("The triangle is a " + classify_triangle(3,4,5) + " triangle.")
---
+++ hw_01/test_hw_01.py
@@ -0,0 +1,50 @@
+import unittest
+from hw_01 import classify_triangle
+
+class Test(unittest.TestCase):
+    # ================= EQUILATERAL TRIANGLES ==================
+    def test01(self):
+        self.assertEqual(classify_triangle(1, 1, 1), 'equilateral')
+    
+    def test02(self):
+        self.assertEqual(classify_triangle(2, 2, 2), 'equilateral')
+
+    def test03(self):
+        self.assertEqual(classify_triangle(100, 100, 100), 'equilateral')
+
+    # ================= ISOSCELES TRIANGLES ==================
+
+    def test04(self):
+        self.assertEqual(classify_triangle(1, 2, 2), 'isosceles')
+    
+    def test05(self):
+        self.assertEqual(classify_triangle(2, 3, 3), 'isosceles')
+
+    def test06(self):
+        self.assertEqual(classify_triangle(100, 200, 200), 'isosceles')
+
+    # ================= SCALENE TRIANGLES ==================
+
+    def test07(self):
+        self.assertEqual(classify_triangle(1, 2, 3), 'scalene')
+    
+    def test08(self):
+        self.assertEqual(classify_triangle(2, 3, 4), 'scalene')
+
+    def test09(self):
+        self.assertEqual(classify_triangle(100, 200, 300), 'scalene')
+
+    # ================= RIGHT TRIANGLES ==================
+
+    def test10(self):
+        self.assertEqual(classify_triangle(3, 4, 5), 'right')
+    
+    def test11(self):
+        self.assertEqual(classify_triangle(6, 8, 10), 'right')
+
+    def test12(self):
+        self.assertEqual(classify_triangle(9, 12, 15), 'right')
+
+
+if __name__ == "__main__":
+    unittest.main()
